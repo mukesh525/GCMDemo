f74759fbdc34fe52b10bb7fb394fda58a6fcd461
Mukesh Jha <mukeshjha2007@gmail.com>
2016-02-16 16:03:59 +0530

    1st commit

--- app/src/main/java/androidwarriors/gcmdemo/PushNotificationService.java
+++ app/src/main/java/androidwarriors/gcmdemo/PushNotificationService.java
@@ -1,17 +1,43 @@
 package androidwarriors.gcmdemo;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
-/**
- * Created by kundan on 10/22/2015.
- */
+
 public class PushNotificationService extends GcmListenerService{
+
+    private NotificationManager mNotificationManager;
+    public static int NOTIFICATION_ID=1;
+    private String TAG="GCMPRO";
 
     @Override
     public void onMessageReceived(String from, Bundle data) {
         String message = data.getString("message");
         //createNotification(mTitle, push_msg);
+    }
+    private void sendNotification(String msg) {
+        Log.d(TAG, "Preparing to send notification...: " + msg);
+        mNotificationManager = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+                this).setSmallIcon(R.drawable.ic_backup)
+                .setContentTitle("GCM Notification")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+        Log.d(TAG, "Notification sent successfully.");
     }
 }
