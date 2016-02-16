5f37a643445530153cc74f9b5c0e3570041b0a5f
Mukesh Jha <mukeshjha2007@gmail.com>
2016-02-16 18:01:53 +0530

    1st commit

--- app/src/main/java/androidwarriors/gcmdemo/PushNotificationService.java
+++ app/src/main/java/androidwarriors/gcmdemo/PushNotificationService.java
@@ -1,44 +1,89 @@
 package androidwarriors.gcmdemo;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class PushNotificationService extends GcmListenerService{
+
+public class PushNotificationService extends GcmListenerService {
 
     private NotificationManager mNotificationManager;
-    public static int NOTIFICATION_ID=1;
-    private String TAG="GCMPRO";
+    public static int NOTIFICATION_ID = 1;
+    private String TAG = "GCMPRO";
 
     @Override
     public void onMessageReceived(String from, Bundle data) {
         String message = data.getString("message");
-        //createNotification(mTitle, push_msg);
         sendNotification(message);
+
     }
+
     private void sendNotification(String msg) {
-        Log.d(TAG, "Preparing to send notification...: " + msg);
-        mNotificationManager = (NotificationManager) this
-                .getSystemService(Context.NOTIFICATION_SERVICE);
+        //Create notification object and set the content.
+        NotificationCompat.Builder nb = new NotificationCompat.Builder(this);
+        nb.setSmallIcon(R.drawable.ic_backup);
+        nb.setContentTitle("Set your title");
+        nb.setContentText("Set Content text");
+        nb.setTicker("Set Ticker text");
+        nb.setContentText(msg);
 
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, MainActivity.class), 0);
+        //get the bitmap to show in notification bar
+        // Bitmap bitmap_image = BitmapFactory.decodeResource(this.getResources(), R.drawable.drawerr);
+        Bitmap bitmap_image = getBitmapFromURL("http://images.landscapingnetwork.com/pictures/images/500x500Max/front-yard-landscaping_15/front-yard-hillside-banyon-tree-design-studio_1018.jpg");
+        NotificationCompat.BigPictureStyle s = new NotificationCompat.BigPictureStyle().bigPicture(bitmap_image);
+        s.setSummaryText("Summary text appears on expanding the notification");
+        nb.setStyle(s);
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
-                this).setSmallIcon(R.drawable.ic_backup)
-                .setContentTitle("GCM Notification")
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
-                .setContentText(msg);
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder TSB = TaskStackBuilder.create(this);
+        TSB.addParentStack(MainActivity.class);
 
-        mBuilder.setContentIntent(contentIntent);
-        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
-        Log.d(TAG, "Notification sent successfully.");
+        // Adds the Intent that starts the Activity to the top of the stack
+        TSB.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                TSB.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        nb.setContentIntent(resultPendingIntent);
+        nb.setAutoCancel(true);
+        NotificationManager mNotificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(11221, nb.build());
+
+
+    }
+
+    public Bitmap getBitmapFromURL(String strURL) {
+        try {
+            URL url = new URL(strURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
